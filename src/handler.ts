b207f647f7badf0c92b6d3606aefb898f9b5a6e7
Kevin KONRAD <konrad.kevin78@gmail.com>
2023-03-06 01:15:41 +0100

    feat(handler): automatically handle try catc

--- src/handler.ts
+++ src/handler.ts
@@ -38,6 +38,10 @@ export abstract class Handler {
   }
 
   private middleware = (req: Request, res: Response, next: NextFunction) => {
-    this.handle(req, res, next);
+    try {
+      this.handle(req, res, next);
+    } catch (err) {
+      next(err);
+    }
   };
 }
